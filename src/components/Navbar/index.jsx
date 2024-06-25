fed603faaba3b6603f93efc05e184bbc8f1046be
ArthurSantDev <devarthursant@gmail.com>
2024-06-25 16:45:17 -0300

    add mobile navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,26 +1,38 @@
-import { Link } from 'react-router-dom'
-import { Link as ScrollLink } from 'react-scroll'
-import './index.css'
+import { Link } from 'react-router-dom';
+import { Link as ScrollLink } from 'react-scroll';
+import React, { useState } from 'react';
+import './index.css';
 
 const one = "<";
 const two = " />";
 
-function index() {
+function Index() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav>
       <div className='Navbar shadow'>
         <div className='nav_title'>
           <Link to='/'>{one}ArthurSant{two}</Link>
         </div>
-        <div className='nav_links'>
-          <ScrollLink to='Home'>      HOME      </ScrollLink>
-          <ScrollLink to='About'>     ABOUT     </ScrollLink>
-          <ScrollLink to='Projects'>  PROJECTS  </ScrollLink>
-          <ScrollLink to='Footer'>    CONTACTS  </ScrollLink>
+        <div className='nav_toggle' onClick={toggleMenu}>
+          <span className='bar'></span>
+          <span className='bar'></span>
+          <span className='bar'></span>
+        </div>
+        <div className={`nav_links ${isOpen ? 'open' : ''}`}>
+          <ScrollLink to='Home' onClick={toggleMenu}>     HOME      </ScrollLink>
+          <ScrollLink to='About' onClick={toggleMenu}>    ABOUT     </ScrollLink>
+          <ScrollLink to='Projects' onClick={toggleMenu}> PROJECTS  </ScrollLink>
+          <ScrollLink to='Footer' onClick={toggleMenu}>   CONTACTS  </ScrollLink>
         </div>
       </div>
     </nav>
-  )
+  );
 }
 
-export default index
+export default Index;
